8e5414e7e2f01699f4e10e77b4e4711431614af9
Gael Helle <gaelhelle@Gaels-VisualPing-MacBook-Pro.local>
2025-09-26 13:11:29 -0700

    add notice

--- nodes/Visualping/VisualpingTrigger.node.ts
+++ nodes/Visualping/VisualpingTrigger.node.ts
@@ -69,6 +69,12 @@ export class VisualpingTrigger implements INodeType {
 					},
 				],
 			},
+			{
+				type: 'notice',
+				name: 'notice',
+				displayName: 'New to Visualping? <a href="https://visualping.io/" target="_blank">Create your account here</a> and explore our <a href="https://docs.visualping.io/n8n/" target="_blank">n8n help guide</a> to get started.',
+				default: 'notice',
+			}
 		],
 		webhooks: [
 			{
